7686313c8f11a2985bfcb5c6c6566c3ee20e3462
vmargichev <93369733+vmargichev@users.noreply.github.com>
2024-02-24 19:13:04 +0200

    Completed second part of the first task

--- first_exercise/main.py
+++ first_exercise/main.py
@@ -1,4 +1,13 @@
 class CryptoGraphy:
+    def read_from_file(file_path):
+        with open(file_path, 'r') as file:
+            return file.read().rstrip()
+        
+    def save_to_file(string_input):
+        with open('file_new.txt', 'w') as file:
+            file.write(string_input)
+        return None
+
 
     def convert_string_to_bytes_utf8(input_string):
         return input_string.encode('utf-8')
@@ -9,8 +18,9 @@ class CryptoGraphy:
 
 
 
-
-byte_array = CryptoGraphy.convert_string_to_bytes_utf8('Упражнение 1 по КМЗИ')
+# first_exercise/file.txt
+byte_array = CryptoGraphy.convert_string_to_bytes_utf8(CryptoGraphy.read_from_file('/home/margichev/workspace/github.com/username/CryptographicMethods/first_exercise/file.txt'))
 print(byte_array)
 string_output = CryptoGraphy.convert_bytes_to_string_utf8(byte_array)
 print(string_output)
+CryptoGraphy.save_to_file(string_output)
